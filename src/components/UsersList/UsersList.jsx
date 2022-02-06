0998cafaba32e77d220985487bd6f85afab98ada
Dawid Gola <dawidgola@MacBook-Pro-Dawid.local>
2022-02-06 15:08:09 +0100

    css-modules

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { users } from 'data/users';
 import UsersListItem from 'components/UsersListItem/UsersListItem';
+import styles from './UsersList.module.scss';
 
 const UsersList = (props) => {
   return (
-    <div>
+    <div className={[styles.container, styles.hasBorder].join(' ')}>
       <ul>
         {users.map((userData, index) => (
           <UsersListItem {...userData} index={index} />
--- src/components/UsersListItem/UsersListItem.jsx
+++ src/components/UsersListItem/UsersListItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './UserListItem.module.scss';
 
 const UsersListItem = ({ index, average, name, attendance = '0%' }) => {
   return (
-    <li key={index}>
+    <li className={styles.container} key={index}>
       <div>{average}</div>
       <div>{name}</div>
       <p>attendance:{attendance}</p>
