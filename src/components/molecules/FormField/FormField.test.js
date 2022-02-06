f48ca75f2d260736ae31a6c57ba11a9e02e03fa3
Dawid Gola <dawidgola@MacBook-Pro-Dawid.local>
2022-02-06 21:39:22 +0100

    add-first-test

--- src/components/molecules/FormField/FormField.test.js
+++ src/components/molecules/FormField/FormField.test.js
@@ -1,40 +1,13 @@
 import React, { useState } from 'react';
 import '@testing-library/jest-dom/extend-expect';
-
 import { render, screen, fireEvent } from '@testing-library/react';
-
-const InputWithButton = () => {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (e) => setInputValue(e.target.value);
-
-  return (
-    <>
-      <input value={inputValue} onChange={handleInputChange} name="Name" id="name" placeholder="enter your name" />
-      <button disabled={!inputValue}>Submit</button>
-    </>
-  );
-};
+import FormField from './FormField';
+import { renderWithProviders } from 'helpers/renderWithThemeProvider';
 
 describe('Input with Button', () => {
   it('Renders the component', () => {
-    render(<InputWithButton />);
-    screen.getByText('Submit');
-  });
+    renderWithProviders(<FormField label="Name" id="name" name="name" />);
 
-  it('Propery handles value change', () => {
-    render(<InputWithButton />);
-    const input = screen.getByPlaceholderText('enter your name');
-    fireEvent.change(input, { target: { value: 'dawid' } });
-    expect(input).toHaveValue('dawid');
-  });
-
-  it('Button is disabled ?', () => {
-    render(<InputWithButton />);
-    const button = screen.getByText('Submit');
-    expect(button).toBeDisabled();
-    const input = screen.getByPlaceholderText('enter your name');
-    fireEvent.change(input, { target: { value: 'dawid' } });
-    expect(button).not.toBeDisabled();
+    // screen.getByText('Submit');
   });
 });
