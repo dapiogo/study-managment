1ab1189344b068995882bb0cbc48ff41bb7650b4
Dawid Gola <dawidgola@MacBook-Pro-Dawid.local>
2022-02-09 20:25:01 +0100

    add useReducer

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useReducer } from 'react';
 import FormField from 'components/molecules/FormField/FormField';
 // import { Button } from 'components/atoms/Button/Button';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
@@ -10,32 +10,77 @@ const initialFormState = {
   name: '',
   attendance: '',
   average: '',
+  consent: false,
+  error: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case 'RESET':
+      return initialFormState;
+    case 'CONSENT':
+      return {
+        ...state,
+        consent: !state.consent,
+      };
+    case 'THROW_ERROR':
+      return {
+        ...state,
+        error: action.errorValue,
+      };
+    default:
+      return state;
+  }
 };
 
 const AddUser = () => {
-  const [formValues, setFormValues] = useState(initialFormState);
+  const [formValues, dispatch] = useReducer(reducer, initialFormState);
+  //   const [formValues, setFormValues] = useState(initialFormState);
   const context = useContext(UsersContext);
 
   const handleInputChange = (e) => {
-    setFormValues({
-      ...formValues,
-      [e.target.name]: e.target.value,
+    dispatch({
+      type: 'INPUT_CHANGE',
+      field: e.target.name,
+      value: e.target.value,
     });
+    // setFormValues({
+    //   ...formValues,
+    //   [e.target.name]: e.target.value,
+    // });
   };
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
-    context.handleAddUser(formValues);
-    setFormValues(initialFormState);
+    if (formValues.consent) {
+      context.handleAddUser(formValues);
+      dispatch({ type: 'RESET' });
+    } else {
+      dispatch({ type: 'THROW_ERROR', errorValue: 'You need to check consent' });
+    }
   };
 
   return (
     <ViewWrapper as="form" onSubmit={handleSubmitUser}>
-      <Title>Add new student</Title>
+      <Title>Add new student </Title>
       <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
       <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputChange} />
       <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
+      <FormField
+        label="Consent"
+        id="consent"
+        name="consent"
+        type="checkbox"
+        value={formValues.consent}
+        onChange={() => dispatch({ type: 'CONSENT' })}
+      />
       <Button type="submit">add</Button>
+      {formValues.error ? <p>{formValues.error}</p> : null}
     </ViewWrapper>
   );
 };
